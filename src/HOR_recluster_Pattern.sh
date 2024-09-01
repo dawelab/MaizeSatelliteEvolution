f3e6036cdbda200e92a93f7e7aab1e53067115e8
RebeccaPiri <54945753+RebeccaPiri@users.noreply.github.com>
2024-09-01 19:20:28 -0400

    Update HOR_recluster_Pattern.sh

--- src/HOR_recluster_Pattern.sh
+++ src/HOR_recluster_Pattern.sh
@@ -8,9 +8,10 @@ mkdir HOR_newpatt
 
 while read lab #group info
 do
-for clust in .9 .91 .92 .93 .94 .95 .96 .97 .98 .99
+for clust in .9 .91 .92 .93 .94 .95 .96 .97 .98 .99 #threshold
 do
 
+#group all those consensus monomers and compare with blat
 cat HOR_clust/*_"$lab"_"$clust".fasta > HOR_newpatt/all_"$lab"_"$clust".fasta
 awk '/^>/{f=!d[$1];d[$1]=1}f' HOR_newpatt/all_"$lab"_"$clust".fasta > HOR_newpatt/all_fix_"$lab"_"$clust".fasta
 blat HOR_newpatt/all_fix_"$lab"_"$clust".fasta HOR_newpatt/all_fix_"$lab"_"$clust".fasta -t=dna -q=dna -maxGap=10 -minScore=0 -repMatch=2147483647 HOR_newpatt/HOR_all_"$lab"_"$clust".fasta.blat
@@ -19,6 +20,9 @@ grep '^[0-9]' HOR_newpatt/HOR_all_"$lab"_"$clust".fasta.blat > HOR_newpatt/sub_H
 done #clust
 done<labels_AB10 labels
 
+#send those similarity scores to R scripts that will 
+#1. convert to matrix, then network, then label monomers by cluster group (number here)
+# Then 2. turn those numbers into letters. Make a character string descirbing each bins. And finally, identify to kmers of interest
 module load R/4.1.2-foss-2021b
 Rscript --vanilla /scratch/rdp22327/Dawe/scaffolding/Mo17_scaff/HOR_newpatt.R /scratch/rdp22327/Dawe/scaffolding/Mo17_scaff/HOR_newpatt
 Rscript --vanilla /scratch/rdp22327/Dawe/scaffolding/Mo17_scaff/HOR_newpatt_repatt.R /scratch/rdp22327/Dawe/scaffolding/Mo17_scaff/HOR_newpatt

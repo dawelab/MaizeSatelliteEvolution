3a436a69aacf1e0fd3b7a868f07c32053a5648e7
RebeccaPiri <54945753+RebeccaPiri@users.noreply.github.com>
2024-09-03 14:37:56 -0400

    Update Array_Group_summaries.sh

--- src/Array_Group_summaries.sh
+++ src/Array_Group_summaries.sh
@@ -34,4 +34,19 @@ cat Ab10_arrays_tail_grouped | awk -v gen="$g" '{if($12 == gen) print $4"\t"$5"\
 bedtools getfasta -fi /scratch/rdp22327/Dawe/scaffolding/Mo17_scaff/"$lin"/ragtag.scaffold.fasta -bed conserved_positions/"$g"_arrays.bed > conserved_positions/"$g"_arrays.fasta 
 done<labels_AB10
 
-#
+####
+#dot plot-- each monomer similarity to consensus for rough pattern plotting
+makehmmerdb "$g"_arrays.fasta "$g"_arrays.fasta.db 
+
+for element in CentC Cent4 knob180 TR1
+do
+##finding monomers, comparing to reference mono
+nhmmer -o "$g"_"$element".out  /scratch/rdp22327/Dawe/consensus/"$element"_consensus_fix_RC.fasta.afa.sto.hmm  "$g"_arrays.fasta.db   
+cat  "$g"_"$element".out  | awk '{print $4"\t"$5"\t"$6}'| grep "chr" |  awk '{if($3>$2) {print $1"\t"$2"\t"$3} else {print $1"\t"$3"\t"$2}}'| bedtools sort -i - > "$g"_"$element".out.bed
+bedtools getfasta -fi "$g"_arrays.fasta -bed "$g"_"$element".out.bed > mono_"$g"_"$element".fasta
+blat  mono_"$g"_"$element".fasta /scratch/rdp22327/Dawe/consensus/"$element"_main_consensus.fasta -t=dna -q=dna -maxGap=10 -minScore=0 -repMatch=2147483647  consensus_mono_"$g"_"$element".fasta.fa.blat
+awk '$1 ~ /^[0-9]*$/' consensus_mono_"$g"_"$element".fasta.fa.blat | awk 'NR>1' | awk '{print $14"\t"($1/(($11/2)+$15-$1))}' > consensus_mono_"$g"_"$element".fasta.fa.blat.sub
+awk -v var="$element" '{print $0"\t"var}' consensus_mono_"$g"_"$element".fasta.fa.blat.sub >nam_consensus_mono_"$g"_"$element".fasta.fa.blat.sub
+done
+cat nam_consensus_mono_"$g"_*.fasta.fa.blat.sub > all_nam_consensus_mono_"$g".fasta.fa.blat.sub
+
